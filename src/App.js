606e5404d7a49772a8251dae499bb68f4fcece8c
RockyBaura <251drougnov@gmail.com>
2023-03-02 21:19:57 +0600

    Filter card on input change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import CardList from "./components/CardList";
+import { robots } from "./data/robots";
 
-class App extends React.Component {
-  render(){
-    return (
-      <CardList />
-    );
+const App =()=> {
+  const [robotData, setRobotData] = useState(robots);
+
+  function onSearchChange(event) {
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(event.target.value.toLowerCase());
+    });
+  
+    setRobotData(filteredRobots);
   }
+
+  return(
+    <>
+      <header>
+        <h1>Robofriends</h1>
+        <input type="text" onChange={onSearchChange} placeholder="Search robots..."/>
+      </header>
+      <hr/>
+      <main>
+        <CardList robotData={robotData} />
+      </main>
+    </>
+  )
 }
 
 export default App;
