d82b9731f3cf19a416cddc604e18594aa54cc78b
RockyBaura <251drougnov@gmail.com>
2023-03-02 23:09:03 +0600

    Classnames & empty text updated

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,9 @@ import Card from './Card';
 
 const CardList = ({robotData}) => {
     return (
-        <ul>
+        <ul className='card-list'>
             {!robotData || robotData.length === 0 ?
-            <h2 className='empty'>No robofriends available</h2> :
+            <h2 className='empty'>No results found.</h2> :
             robotData.map(item=>(
                 <Card
                     key={item.id}
