17e74184a911ceed510bdc51d6ee414952d55dd4
RockyBaura <251drougnov@gmail.com>
2023-03-02 23:08:29 +0600

    Added preloader text for img

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,31 @@
-const Card = ({id, name, email}) => {
+import { useState } from "react";
+
+const Card = ({ id, name, email }) => {
+    const [loading, setLoading] = useState(true);
+
+    const handleImageLoad = () => {
+        setLoading(false);
+    };
+
     return (
-        <li>
-            <div>
-                <img src={`https://robohash.org/${id}?size=200x200`} alt={`${name}'s portrait`} />
-            </div>
-            <section>
-                <h2>{name}</h2>
-                <a href={`mailto:${email}`} target="_blank" rel="noreferrer">{email}</a>
-            </section>
+        <li className="card">
+        <div className="card__img">
+            {loading && <div className="card__img-placeholder">Loading...</div>}
+            <img
+            src={`https://robohash.org/${id}?size=200x200`}
+            alt={`${name}'s portrait`}
+            onLoad={handleImageLoad}
+            style={{ display: loading ? "none" : "block" }}
+            />
+        </div>
+        <section className="card__text">
+            <h2 className="card__text-header">{name}</h2>
+            <a className="card__text-link" href={`mailto:${email}`} target="_blank" rel="noreferrer">
+            {email}
+            </a>
+        </section>
         </li>
-    )
-}
+    );
+};
 
 export default Card;
