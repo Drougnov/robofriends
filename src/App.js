e60a73f3d7bd159f8e9636015cccb57180a83db0
RockyBaura <251drougnov@gmail.com>
2023-03-04 22:09:17 +0600

    Used api to get robot-data  | Added filtering by email

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "./components/CardList";
-import { robots } from "./data/robots";
 
 const App =()=> {
-  const [robotData, setRobotData] = useState(robots);
+  const [robotData, setRobotData] = useState([]);
+  const [originalRobotData, setOriginalRobotData] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => {
+      setRobotData(data)
+      setOriginalRobotData(data)
+    })
+  },
+  [])
 
   function onSearchChange(event) {
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(event.target.value.toLowerCase());
+    const inputValue = event.target.value.toLowerCase();
+    const filteredRobots = originalRobotData.filter((robot) => {
+      return robot.name.toLowerCase().includes(inputValue) || robot.email.toLowerCase().includes(inputValue);
     });
   
     setRobotData(filteredRobots);
